a68a53f0170cdf4a38ba67794f6893b5849efd28
GUILHERME DE OLIVEIRA LIMA <22282086@CFP127ALUNO.LOCAL>
2022-09-13 11:29:14 -0300

    saf

--- src/br/senai/sp/jandira/AppTabuada.java
+++ src/br/senai/sp/jandira/AppTabuada.java
@@ -14,29 +14,30 @@ public class AppTabuada {
 		tela.titulo = "Tabuada";
 		tela.altura = 600;
 		tela.largura = 600;
-		tela.corDeFundoDaTela = new Color(44, 185, 204);
+		tela.corDeFundoDaTela = new Color(230, 230, 230);
 		tela.fonteDosLabels = new Font("Inter", Font.BOLD, 15);
 		tela.corDoBotao = new Color (106, 253, 81);
 		tela.corDoBotao2 = new Color (255, 45, 50);
-//		tela.corDeTextoDoBotao = new Color(0, 0, 0);
+		tela.corDeTextoDoBotao2 = new Color(0, 0, 0);
+		tela.corDeTextoDoBotao = new Color(0, 0, 0);
 		tela.criarTela();
 		
 		
-		Tabuada tabuada = new Tabuada();
-		tabuada.multiplicando = 2;
-		tabuada.minimoMultiplicador = 8;
-		tabuada.maximoMultiplicador = 14;
-		
-		String[] resultado = tabuada.getTabuada();
-		
-		int contador = 0;
-		while (contador < resultado.length) {
-			System.out.println(resultado[contador]);
-			contador++;
-		}
-		
-		System.out.println("------------Fim---------------");
-		
+//		Tabuada tabuada = new Tabuada();
+//		tabuada.multiplicando = 2;
+//		tabuada.minimoMultiplicador = 8;
+//		tabuada.maximoMultiplicador = 14;
+//		
+//		String[] resultado = tabuada.getTabuada();
+//		
+//		int contador = 0;
+//		while (contador < resultado.length) {
+//			System.out.println(resultado[contador]);
+//			contador++;
+//		}
+//		
+//		System.out.println("------------Fim---------------");
+//		
 	}
 	
 }
--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -5,12 +5,15 @@ import java.awt.Container;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
@@ -26,7 +29,8 @@ public class FrameTabuada {
 	public Font fonteDosLabels;
 	public Color corDoBotao;
 	public Color corDoBotao2;
-//	public Color corDeTextoDoBotao;
+	public Color corDeTextoDoBotao2;
+	public Color corDeTextoDoBotao;
 	
 	public void criarTela() {
 		
@@ -36,24 +40,27 @@ public class FrameTabuada {
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setLayout(null);										
 		
+		
 		// Obter a instância do painel de conteúdo(container)
 		Container painel = tela.getContentPane();
+		painel.setBackground(corDeFundoDaTela);
+		
 	
+		
 		// Crias os componentes da tela e colocar no painel(container)
 		JLabel labelTabuada = new JLabel();
 		labelTabuada.setText("Tabuada 1.0");
 		labelTabuada.setBounds(70, 10, 600, 30);
-		labelTabuada.setFont(new Font("Inter", Font.BOLD, 25));
+		labelTabuada.setFont(new Font("Inter", Font.BOLD, 27));
 		labelTabuada.setForeground(Color.red);
 		
+		
 		JLabel labelTexto = new JLabel();
-		labelTexto.setText("Com a tabuada 1.0 os seus problemas acabaram. Calcule a tabuada que desejar em segundos!\r\n"+ "");
+		labelTexto.setText("Com a tabuada 1.0 os seus problemas acabaram. Calcule a tabuada que desejar em segundos!\r\n");
 		labelTexto.setBounds(70, 45, 600, 30);
 		labelTexto.setFont(new Font("Calibri", Font.BOLD, 12));
-//		labelTexto.setForeground(Color.black);
+		labelTexto.setForeground(Color.black);
 		
-//		JLabel labelIcon = new JLabel();
-//		labelIcon.setIcon("");
 		
 		JLabel labelMultiplicando = new JLabel();
 		labelMultiplicando.setText("Multiplicando:");
@@ -61,8 +68,10 @@ public class FrameTabuada {
 		labelMultiplicando.setFont(fonteDosLabels);
 		labelMultiplicando.setForeground(Color.black);
 		
+		
 		JTextField textFieldMultiplicando = new JTextField();
 		textFieldMultiplicando.setBounds(250, 100, 200, 30);
+		
 		
 		JLabel labelminimoMultiplicador = new JLabel();
 		labelminimoMultiplicador.setText("Mínimo Multiplicador:");
@@ -70,8 +79,10 @@ public class FrameTabuada {
 		labelminimoMultiplicador.setFont(fonteDosLabels);
 		labelminimoMultiplicador.setForeground(Color.black);
 		
+		
 		JTextField textFieldminimoMultiplicador = new JTextField();
 		textFieldminimoMultiplicador.setBounds(250, 150, 200, 30);
+		
 		
 		JLabel labelmaximoMultiplicador = new JLabel();
 		labelmaximoMultiplicador.setText("Máximo Multiplicador:");
@@ -79,18 +90,23 @@ public class FrameTabuada {
 		labelmaximoMultiplicador.setFont(fonteDosLabels);
 		labelmaximoMultiplicador.setForeground(Color.black);
 		
+		
 		JTextField textFieldmaximoMultiplicador = new JTextField();
 		textFieldmaximoMultiplicador.setBounds(250, 200, 200, 30);
+		
 		
 		JButton buttonCalcular = new JButton();
 		buttonCalcular.setText("Calcular");
 		buttonCalcular.setBounds(70, 250, 200, 30);
 		buttonCalcular.setBackground(corDoBotao);
+		buttonCalcular.setForeground(corDeTextoDoBotao);
+		
 		
 		JButton buttonLimpar = new JButton();
 		buttonLimpar.setText("Limpar");
 		buttonLimpar.setBounds(300, 250, 150, 30);
 		buttonLimpar.setBackground(corDoBotao2);
+		buttonLimpar.setForeground(corDeTextoDoBotao2);
 		
 		JLabel labelResultado = new JLabel();
 		labelResultado.setText("Resultado:");
@@ -98,9 +114,11 @@ public class FrameTabuada {
 		labelResultado.setFont(fonteDosLabels);
 		labelResultado.setForeground(Color.black);
 		
-		ImageIcon imagem = new ImageIcon(getClass().getResource("calculadora.png"));
+		
+		ImageIcon imagem = new ImageIcon(getClass().getResource("calcu.png"));
 		JLabel icon = new JLabel(imagem);
-		icon.setBounds(30, 70, 30, 30);
+		icon.setBounds(60, 100, 35, 30);
+		
 		
 		//Lista da tabuada
 		JList<String> lista = new JList<String>();
@@ -116,27 +134,122 @@ public class FrameTabuada {
 				
 				Tabuada tabuada = new Tabuada();
 				
+				
+				
+				
+				
+				if (textFieldmaximoMultiplicador.getText().isEmpty() || textFieldminimoMultiplicador.getText().isEmpty()
+						|| textFieldMultiplicando.getText().isEmpty()) {
+
+				JOptionPane.showMessageDialog(null, "Você esqueceu de digitar os valores. Por favor preencha as opções!","Aviso", JOptionPane.WARNING_MESSAGE);
+				}else {
+				 
 				tabuada.multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
 				tabuada.minimoMultiplicador = Integer.parseInt(textFieldminimoMultiplicador.getText());
 				tabuada.maximoMultiplicador = Integer.parseInt(textFieldmaximoMultiplicador.getText());
+				}
 				
+				if (tabuada.maximoMultiplicador <= tabuada.minimoMultiplicador) {
+					JOptionPane.showMessageDialog(null, "Você colocou o Maximo multiplicador menor que o Minimo Multiplicador.Preencha corretamente! ","Aviso", JOptionPane.WARNING_MESSAGE);
+					}else {
 				String[] resultado = tabuada.getTabuada();
 				lista.setListData(resultado);
 				
 				scroll.setVisible(true);
+					}
+				
 				
 			}
 		});
 		
+		//Função para limpar as caixas de texto
 		buttonLimpar.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				
-				scroll.setVisible(false);
+				String[] limpar = {" "};
+				lista.setListData(limpar);
 				textFieldMultiplicando.setText("");
 				textFieldminimoMultiplicador.setText("");
 				textFieldmaximoMultiplicador.setText("");
+				
+			}
+		});
+		
+		//Função para impedir de digitar letras
+		
+		
+		textFieldmaximoMultiplicador.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				textFieldmaximoMultiplicador.setText(textFieldmaximoMultiplicador.getText().replaceAll("[^0-9]", ""));
+				if (textFieldmaximoMultiplicador.getText().length() > 9) {
+					
+				}
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		textFieldminimoMultiplicador.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				textFieldminimoMultiplicador.setText(textFieldminimoMultiplicador.getText().replaceAll("[^0-9]", ""));
+				if (textFieldminimoMultiplicador.getText().length() > 9) {
+					
+				}
+				
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		textFieldMultiplicando.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				textFieldMultiplicando.setText(textFieldMultiplicando.getText().replaceAll("[^0-9]", ""));
+				if (textFieldMultiplicando.getText().length() > 9) {
+					
+				}
+				
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
 				
 			}
 		});
